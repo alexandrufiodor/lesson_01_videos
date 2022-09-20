fa086971b441dbc32fb14cdd9a688d58bb94d46c
alexandrufiodor <alexandrufiodor95@gmail.com>
2022-09-20 22:40:26 +0300

    validation

--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -5,13 +5,12 @@ import {body} from "express-validator";
 
 export const videosRouter = Router()
 
-const titleValidation = body('title').exists().withMessage('Title is required').isString().withMessage('Title should be a string').trim().isLength({max: 40}).withMessage('Title should be maxim 40 length');
+const titleValidation = body('title').exists().withMessage('Title is required').isString().withMessage('Title should be a string').trim().isLength({min: 1, max: 40}).withMessage('Title should be minim 1 and maxim 40 length');
 const authorValidation = body('author').exists().withMessage('Author is required').isString().withMessage('Author should be a string');
 const minAgeRestriction = body('minAgeRestriction').isInt({ min: 1, max: 18 }).withMessage('minAgeRestriction value must be between 1 to 18').optional();
 const canBeDownloadedValidation = body('canBeDownloaded').isBoolean().withMessage('canBeDownloaded should be boolean').optional();
 const availableResolutionsValidation = body('availableResolutions').isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]).withMessage('availableResolutions should be include ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"] ').optional();
-const publicationDateValidation = body('publicationDate').isDate().withMessage('Publication Date should be a date').optional();
-const createdAtDateValidation = body('createdAt').isDate().withMessage('createdAt should be a date').optional();
+const publicationDateValidation = body('publicationDate').isString().withMessage('Publication Date should be a date').optional();
 
 videosRouter.get(`/`, (req: Request, res: Response) => {
     const foundedVideos = videosRepository.findVideos(req.query.title?.toString())
@@ -32,11 +31,11 @@ videosRouter.delete(`/:id`, (req: Request, res: Response) => {
     }
     return res.send(404)
 })
-videosRouter.post(`/`, titleValidation, minAgeRestriction, authorValidation, canBeDownloadedValidation, availableResolutionsValidation, createdAtDateValidation, publicationDateValidation, validationMiddleware, (req: Request, res: Response) => {
+videosRouter.post(`/`, titleValidation, minAgeRestriction, authorValidation, canBeDownloadedValidation, availableResolutionsValidation, publicationDateValidation, validationMiddleware, (req: Request, res: Response) => {
     const newVideo = videosRepository.addNewVideo(req.body.title, req.body.author, req.body.availableResolutions || [], req.body.canBeDownloaded || false, req.body.minAgeRestriction || null)
     res.status(201).send(newVideo)
 })
-videosRouter.put(`/:id`, titleValidation, minAgeRestriction, authorValidation, canBeDownloadedValidation, availableResolutionsValidation, createdAtDateValidation, publicationDateValidation, validationMiddleware, (req: Request, res: Response) => {
+videosRouter.put(`/:id`, titleValidation, minAgeRestriction, authorValidation, canBeDownloadedValidation, availableResolutionsValidation, publicationDateValidation, validationMiddleware, (req: Request, res: Response) => {
     const isUpdated = videosRepository.updateVideo(+req.params.id, req.body.title, req.body.author, req.body.availableResolutions || [], req.body.canBeDownloaded || false, req.body.minAgeRestriction || null )
     if (isUpdated) {
         res.status(204)
