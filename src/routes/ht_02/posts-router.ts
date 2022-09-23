74476981f30ccbe8748b11f063342291d7afd4e8
alexandrufiodor <alexandrufiodor95@gmail.com>
2022-09-23 23:19:29 +0300

    ht_02

--- src/routes/ht_02/posts-router.ts
+++ src/routes/ht_02/posts-router.ts
@@ -1,21 +1,27 @@
 import {Request, Response, Router} from "express";
 import {validationMiddleware} from "../../middlewares/validation-middleware";
 import {body} from "express-validator";
-import {blogsRepository} from "../../repositories/ht_02/blogs-repository";
+
 import {authorization} from "./users";
 import {posts, postsRepository} from "../../repositories/ht_02/posts-repository";
+import {blogs, blogsRepository} from "../../repositories/ht_02/blogs-repository";
 
 export const postsRouter = Router()
 const titleValidation = body('title').exists().withMessage('Title is required').isString().withMessage('Title should be a string').trim().isLength({max: 30}).withMessage('Title should be maxim 30 length');
 const contentValidation = body('content').exists().withMessage('Content is required').isString().withMessage('Content should be a string').trim().isLength({max: 1000}).withMessage('Content should be maxim 1000 length');
 const shortDescriptionValidation = body('shortDescription').exists().withMessage('Description is required').isString().withMessage('Name should be a string').trim().isLength({max: 100}).withMessage('Name should be maxim 100 length');
-const blogIdValidation = body('blogId').exists().withMessage('Blog id is required').isString().withMessage('Blog Id should be a string');
-const blogIdNotFoundValidation = () => {
-    return {errorsMessages: [{
-            message: 'Blog Id not founded',
-            field: 'blogId'
-        }]}
-}
+const blogIdValidation = body('blogId').exists().withMessage('Blog id is required').isString().withMessage('Blog Id should be a string').isIn(blogs).withMessage('Blog Id not founded');
+const blogIdNotFoundValidation = body("blogId").custom((blogId: string) => {
+    if (blogId?.length > 0) {
+        const findBlog = blogsRepository.findBlogById(blogId)
+        if (findBlog) {
+            return true
+        }
+        throw new Error('BlogId not found');
+    } else {
+        throw new Error('BlogId is required');
+    }
+})
 
 postsRouter.get(`/`, (req: Request, res: Response) => {
     const foundedPosts = postsRepository.findPosts(req.query.title?.toString())
@@ -36,17 +42,18 @@ postsRouter.delete(`/:id`, authorization(), (req: Request, res: Response) => {
     }
     return res.send(404)
 })
-postsRouter.post(`/`, authorization(), titleValidation, contentValidation, shortDescriptionValidation, blogIdValidation, validationMiddleware, (req: Request, res: Response) => {
+postsRouter.post(`/`, authorization(), titleValidation, contentValidation, shortDescriptionValidation, blogIdNotFoundValidation, validationMiddleware, (req: Request, res: Response) => {
     const newPost = postsRepository.addNewPost(req.body.title, req.body.shortDescription, req.body.content, req.body.blogId)
-    if (newPost) {
+    // if (newPost) {
         res.status(201).send(newPost)
         return
-    } else {
-        res.status(400).send(blogIdNotFoundValidation())
-    }
+    // }
+    // else {
+    //     res.status(400).send(blogIdNotFoundValidation())
+    // }
 
 })
-postsRouter.put(`/:id`, authorization(), titleValidation, contentValidation, shortDescriptionValidation, blogIdValidation, validationMiddleware, (req: Request, res: Response) => {
+postsRouter.put(`/:id`, authorization(), titleValidation, contentValidation, shortDescriptionValidation, blogIdNotFoundValidation, validationMiddleware, (req: Request, res: Response) => {
     const isUpdated = postsRepository.updatePost(req.params.id, req.body.title, req.body.shortDescription, req.body.content, req.body.blogId)
     if (isUpdated) {
         res.status(204).send(isUpdated)
