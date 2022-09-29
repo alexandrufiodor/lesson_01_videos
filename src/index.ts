0d6c232adb4757b6a18455f143ee00565093440a
alexandrufiodor <alexandrufiodor95@gmail.com>
2022-09-29 23:35:51 +0300

    ht_03

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {dataRouter} from "./routes/ht_01/clear-data-router";
 import {blogsRouter} from "./routes/ht_02/blogs-router";
 import {postsRouter} from "./routes/ht_02/posts-router";
 import {blogsRouter as blogsRouter_03} from "./routes/ht_03/blogs-router";
-// import {postsRouter as postsRouter_03} from "./routes/ht_03/posts-router";
+import {postsRouter as postsRouter_03} from "./routes/ht_03/posts-router";
 import {runDb} from "./repositories/ht_03/db";
 
 export const app = express()
@@ -22,7 +22,7 @@ app.use('/ht_02/api/posts', postsRouter)
 app.use('/ht_02/api/testing', dataRouter)
 
 app.use('/ht_03/api/blogs', blogsRouter_03)
-// app.use('/ht_03/api/posts', postsRouter_03)
+app.use('/ht_03/api/posts', postsRouter_03)
 app.use('/ht_03/api/testing', dataRouter)
 
 const startApp = async () => {
