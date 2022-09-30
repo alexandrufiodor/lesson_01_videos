e37ba3fc6c0eac541b8dcbc47cd1500fb256ba4b
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2022-09-30 11:05:51 +0300

    Update clear-data-router.ts
    ----

--- src/routes/ht_03/clear-data-router.ts
+++ src/routes/ht_03/clear-data-router.ts
@@ -5,7 +5,11 @@ import {postsRepository} from "../../repositories/ht_03/posts-repository";
 export const dataRouter = Router()
 
 dataRouter.delete(`/all-data`, async (req: Request, res: Response) => {
-    await blogsRepository.deleteAll()
-    await postsRepository.deleteAll()
-    res.send(204)
+    const removeBlogs = await blogsRepository.deleteAll()
+    const removePosts = await postsRepository.deleteAll()
+    if (removeBlogs && removePosts) {
+      res.sendStatus(204)
+        return
+    }
+    res.sendStatus(404)
 })
