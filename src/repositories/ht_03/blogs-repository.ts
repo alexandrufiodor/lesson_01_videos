c1b53fe0fecca45b9982a67a83798e6a364d90c5
alexandrufiodor <alexandrufiodor95@gmail.com>
2022-10-02 21:53:33 +0300

    ht_03

--- src/repositories/ht_03/blogs-repository.ts
+++ src/repositories/ht_03/blogs-repository.ts
@@ -22,29 +22,28 @@ export const blogsRepository = {
         return blogsCollection.find({name: filter.name}).toArray()
     },
     async findBlogById(id: string): Promise<blogsType | null> {
-        const result = await blogsCollection.findOne({id})
+        const result = await blogsCollection.findOne({_id: new Object(id)})
         if (result) {
             return result
         }
         return null
     },
     async removeBlog(id: string): Promise<boolean>  {
-        const result = await blogsCollection.deleteOne({id})
+        const result = await blogsCollection.deleteOne({_id: new Object(id)})
         return result.deletedCount === 1
     },
     async addNewBlog(name: string, youtubeUrl: string): Promise<blogsType> {
         const newBlog = {
-            createdAt: new Date,
             id: (+(new Date)).toString(),
             name,
             youtubeUrl,
+            createdAt: new Date,
         }
         const result = await blogsCollection.insertOne(newBlog)
-        //@ts-ignore
-        return {_id: result.insertedId, ...newBlog}
+        return newBlog
     },
     async updateBlog(id: string, name: string, youtubeUrl: string): Promise<blogsType | boolean> {
-        const result = await blogsCollection.updateOne({id}, {$set: {name, youtubeUrl}})
+        const result = await blogsCollection.updateOne({_id: new Object(id)}, {$set: {name, youtubeUrl}})
         return result.matchedCount === 1
     }
 }
--- src/repositories/ht_03/posts-repository.ts
+++ src/repositories/ht_03/posts-repository.ts
@@ -26,38 +26,36 @@ export const postsRepository = {
         return postsCollection.find({title: filter.title}).toArray()
     },
     async findPostById(id: string): Promise<postsType | null> {
-        const result = await postsCollection.findOne({id})
+        const result = await postsCollection.findOne({_id: new Object(id)})
         if (result) {
             return result
         }
         return null
     },
     async removePost(id: string): Promise<boolean> {
-        const result = await postsCollection.deleteOne({id})
+        const result = await postsCollection.deleteOne({_id: new Object(id)})
         return result.deletedCount === 1
     },
     async addNewPost(title: string, shortDescription: string, content: string, blogId: string): Promise<postsType | null> {
         const blog = await blogsRepository.findBlogById(blogId)
         if (blog) {
             const newPost = {
-                blogId,
-                blogName: blog.name,
                 id: (+(new Date)).toString(),
-                content,
-                createdAt: new Date,
                 title,
                 shortDescription,
+                content,
+                blogId,
+                blogName: blog.name,
+                createdAt: new Date,
             }
             const result = await postsCollection.insertOne(newPost)
-            // return newPost
-            //@ts-ignore
-            return {_id: result.insertedId, ...newPost}
+            return newPost
         } return null
     },
     async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean | null> {
         const blog = await blogsRepository.findBlogById(blogId)
         if (blog) {
-            const result = await postsCollection.updateOne({id}, {$set: {title, shortDescription, content, blogId, blogName: blog.name}})
+            const result = await postsCollection.updateOne({_id: new Object(id)}, {$set: {title, shortDescription, content, blogId, blogName: blog.name}})
             return result.matchedCount === 1
         } return null
     }
